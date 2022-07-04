77083f43a00153127d319d0ab02320517aa4065a
vbezakova <vendula.bezakova@gmail.com>
2022-07-04 22:58:14 +0200

    CSS

--- sperky_minka/sperky_minka/settings.py
+++ sperky_minka/sperky_minka/settings.py
@@ -118,11 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
 STATIC_URL = 'static/'
 MEDIA_URL = "media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
